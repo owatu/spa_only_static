b5467385cfa74dca07495649aa357add7215691c
owatu <oaoto1811@gmail.com>
2022-07-22 15:11:37 +0900

    initialize

---
+++ src/Root.tsx
@@ -0,0 +1,7 @@
+export default function Root() {
+  return (
+    <>
+      <p>nothing here...</p>
+    </>
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import { render } from "solid-js/web"
+import Root from "./Root"
+
+render(() => <Root />, document.querySelector("main") as HTMLElement)
